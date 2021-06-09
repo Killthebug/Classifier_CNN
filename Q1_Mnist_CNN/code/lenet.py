e261cc02386743f0d619a20f2cce59d445c51702
Jaipal Singh <jaipal@Jaipals-MacBook-Air.local>
2021-06-09 17:55:36 +0530

    Added type hints for function and updated how train and test data is read.

--- Q1_Mnist_CNN/code/lenet.py
+++ Q1_Mnist_CNN/code/lenet.py
@@ -13,7 +13,7 @@ class LeNet(nn.Module):
         super().__init__()
 
         self.cnn_model = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),  # (N, 1, 28, 28) -> (N, 6, 24, 24)
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),  # (N, 1, 28, 28) -> (N, 6, 24, 24)
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2, stride=2),  # (N, 6, 24, 24) -> (N, 6, 12, 12)
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),  # (N, 6, 12, 12) -> (N, 6, 8, 8)
