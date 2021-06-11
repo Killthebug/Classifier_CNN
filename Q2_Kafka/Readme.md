9261e67c798f89072cd9cb701898d9f20c7e7a14
Jaipal Singh <jaipal@Jaipals-MacBook-Air.local>
2021-06-11 19:33:46 +0530

    Added basic documentation

---
+++ Q2_Kafka/Readme.md
@@ -0,0 +1,3 @@
+Unified API over Kafka and Google Pub Sub.
+
+Sample interaction script : main.py
--- Q2_Kafka/main.py
+++ Q2_Kafka/main.py
@@ -6,25 +6,36 @@ from service.consumers.gcp_service import GCPService as GConsumer
 
 import sys
 
+"""
+Define config elements of kafka instance to interact with
+"""
 BOOTSTRAP_SERVER = "localhost:9092"
 KAFKA_TOPIC = "numtest"
 
+"""
+Define config elements of Google Pub Sub instance to interact with
+"""
 GCP_PROJECT_ID = "silver-spark-316405"
 GCP_TOPIC = "sending_topic_1"
 GCP_SUBSCRIPTION_ID = "my-sub"
 
-choice = sys.argv[1]
-run_type = sys.argv[2]
+"""
+Dummy Data used for experiment
+"""
+DATA_TO_PUBLISH = "24123"
+
+choice = sys.argv[1]    # Determine which message broker to use : Kafka / Google Pub Sub
+run_type = sys.argv[2]  # Determine behaviour of message broker : Producer / Consumer
 
 if choice == "KAFKA" and run_type == "PRODUCER":
     config = {"bootstrap_server": BOOTSTRAP_SERVER}
     client = KProducer(config=config)
-    client.produce(24123, KAFKA_TOPIC)
+    client.produce(DATA_TO_PUBLISH, KAFKA_TOPIC)
 
 if choice == "GCP" and run_type == "PRODUCER":
     config = {"project_id": GCP_PROJECT_ID}
     client = GProducer(config=config)
-    client.produce(24123, GCP_TOPIC)
+    client.produce(DATA_TO_PUBLISH, GCP_TOPIC)
 
 if choice == "KAFKA" and run_type == "CONSUMER":
     kafka_config = {
