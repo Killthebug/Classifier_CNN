b3e90f02734d96dcc47d2eaae5bb32d4b690d53a
Jaipal Singh <jaipal@Jaipals-MacBook-Air.local>
2021-06-15 21:59:44 +0530

    Updated main function to reflect pass of custom value_serializer() & value_deserializer()

--- Q2_Kafka/main.py
+++ Q2_Kafka/main.py
@@ -5,6 +5,7 @@ from service.producers.gcp_service import GCPService as GProducer
 from service.consumers.gcp_service import GCPService as GConsumer
 
 import sys
+import json
 
 """
 Define config elements of kafka instance to interact with
@@ -24,28 +25,48 @@ Dummy Data used for experiment
 """
 DATA_TO_PUBLISH = "24123"
 
-choice = sys.argv[1]    # Determine which message broker to use : Kafka / Google Pub Sub
+choice = sys.argv[1]  # Determine which message broker to use : Kafka / Google Pub Sub
 run_type = sys.argv[2]  # Determine behaviour of message broker : Producer / Consumer
 
+
+def kafka_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+
+def kafka_deserializer(data):
+    return json.loads(data.decode("utf8"))
+
+
+def gcp_serializer(data):
+    return str(data).encode("utf-8")
+
+
 if choice == "KAFKA" and run_type == "PRODUCER":
-    config = {"bootstrap_server": BOOTSTRAP_SERVER}
-    client = KProducer(config=config)
+    kafka_config = {
+        "bootstrap_server": BOOTSTRAP_SERVER,
+        "value_serializer": kafka_serializer
+    }
+    client = KProducer(config=kafka_config)
     client.produce(DATA_TO_PUBLISH, KAFKA_TOPIC)
 
 if choice == "GCP" and run_type == "PRODUCER":
-    config = {"project_id": GCP_PROJECT_ID}
-    client = GProducer(config=config)
+    gcp_config = {
+        "project_id": GCP_PROJECT_ID,
+        "value_serializer": gcp_serializer
+    }
+    client = GProducer(config=gcp_config)
     client.produce(DATA_TO_PUBLISH, GCP_TOPIC)
 
 if choice == "KAFKA" and run_type == "CONSUMER":
     kafka_config = {
         "name": KAFKA_TOPIC,
         "bootstrap_server": BOOTSTRAP_SERVER,
+        "value_deserializer": kafka_deserializer,
         "auto_offset_reset": "earliest",
         "enable_auto_commit": True,
         "group_id": "counters",
     }
-    client = KConsumer(config=config)
+    client = KConsumer(config=kafka_config)
     while 1:
         client.consume()
 
