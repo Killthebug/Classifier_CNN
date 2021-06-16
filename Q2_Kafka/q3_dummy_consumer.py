62442a5db4c9aa0335ba86df1b0341631a8a3fa7
Jaipal Singh <jaipal@Jaipals-MacBook-Air.local>
2021-06-16 14:49:10 +0530

    Refactoring Global Variables

--- Q2_Kafka/q3_dummy_consumer.py
+++ Q2_Kafka/q3_dummy_consumer.py
@@ -1,8 +1,8 @@
 from service.consumers.kafka_service import KafkaService as KConsumer
 import json
 
-CONSUMER_SERVER = "localhost:9092"
-CONSUMER_TOPIC = "test_response"
+SUBSCRIBER_SERVER = "localhost:9092"
+SUBSCRIBER_TOPIC = "test_response"
 
 
 def data_deserializer(data):
@@ -11,8 +11,8 @@ def data_deserializer(data):
 
 if __name__=="__main__":
     kafka_config = {
-        "name": CONSUMER_TOPIC,
-        "bootstrap_server": CONSUMER_SERVER,
+        "name": SUBSCRIBER_TOPIC,
+        "bootstrap_server": SUBSCRIBER_SERVER,
         "value_deserializer": data_deserializer,
         "auto_offset_reset": "earliest",
         "enable_auto_commit": True,
